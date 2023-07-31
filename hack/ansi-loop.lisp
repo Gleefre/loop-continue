d83e74021e4a4ecfad820c9c7e3081c7d4ff2c46
Gleefre <varedif.a.s@gmail.com>
2023-07-31 18:15:07 +0200

    Add ECL support

--- hack/ansi-loop.lisp
+++ hack/ansi-loop.lisp
@@ -5,10 +5,11 @@
                 #+clasp #:core
                 #+allegro #:excl
                 #+abcl #:loop
-                #:loop-disallow-conditional
-                #:loop-get-form
-                #:loop-emit-body
-                #:loop-error
+                #+ecl #:si
+                #-ecl #:loop-disallow-conditional
+                #-ecl #:loop-get-form
+                #-ecl #:loop-emit-body
+                #-ecl #:loop-error
                 #:*loop-ansi-universe*
                 #-sbcl #:loop-universe-keywords
                 #+sbcl #:keywords)
@@ -20,6 +21,30 @@
 (defmacro uni ()
   #-sbcl '(loop-universe-keywords *loop-ansi-universe*)
   #+sbcl '(slot-value *loop-ansi-universe* 'keywords))
+
+;; ECL "hides" some functions by declaring them (si::c-local)
+#+ecl
+(progn
+  (defun loop-context ()
+    (do ((l si::*loop-source-context* (cdr l)) (new nil (cons (car l) new)))
+        ((eq l (cdr si::*loop-source-code*)) (nreverse new))))
+  (defun loop-error (format-string &rest format-args)
+    (si::simple-program-error "~?~%Current LOOP context:~{ ~S~}."
+                              format-string format-args (loop-context)))
+  (defun loop-pop-source ()
+    (if si::*loop-source-code*
+        (pop si::*loop-source-code*)
+        (loop-error "LOOP source code ran out when another token was expected.")))
+  (defun loop-get-form ()
+    (if si::*loop-source-code*
+        (loop-pop-source)
+        (loop-error "LOOP code ran out where a form was expected.")))
+  (defun loop-emit-body (form)
+    (setf si::*loop-emitted-body* t)
+    (push form si::*loop-body*))
+  (defun loop-disallow-conditional (&optional kwd)
+    (when si::*loop-inside-conditional*
+      (loop-error "~:[This LOOP~;The LOOP ~:*~S~] clause is not permitted inside a conditional." kwd))))
 
 (defun loop-tag ()
   (loop-disallow-conditional :tag)
--- loop-tag-go.asd
+++ loop-tag-go.asd
@@ -4,4 +4,4 @@
   :author "Grolter <varedif.a.s@gmail.com>"
   :license "Apache 2.0"
   :pathname "hack"
-  :components (#+(or sbcl cmucl ccl allegro clasp abcl) (:file "ansi-loop")))
+  :components (#+(or sbcl cmucl ccl allegro clasp abcl ecl) (:file "ansi-loop")))
